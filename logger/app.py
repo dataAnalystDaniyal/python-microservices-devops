537392e89f9d46c5840a9c1f1cefcce49a6a1738
Sayed Danyal <daniyal.syd8@gmail.com>
2025-09-05 09:33:51 +0530

    Update app.py

--- logger/app.py
+++ logger/app.py
@@ -1,1 +1,11 @@
+import time
 
+def log_writer():
+    with open("service.log", "a") as f:
+        while True:
+            f.write("Logger service running...\n")
+            f.flush()
+            time.sleep(5)
+
+if __name__ == "__main__":
+    log_writer()
